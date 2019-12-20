e9db0c6e3b4dde7384caa8edf9a8aeb1d1e9126d
Yash Kalebere <ysk25@drexel.edu>
2019-12-20 02:44:00 -0500

    Another update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ln -sv ~/.dotfiles/.bash_profile ~
+ln -sv ~/.dotfiles/.bashrc ~
 ln -sv ~/.dotfiles/.aliases ~
-ln -sv ~/.dotfiles/.vim ~
 ln -sv ~/.dotfiles/.tmux.conf ~
+ln -sv ~/.dotfiles/.vim/ ~
